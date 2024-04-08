d900f5044d9be0a5c73ef64c66e1d2ab48eb872e
Tsatsu Adogla-Bessa Jnr <tsatsujnr@gmail.com>
2024-04-08 13:49:48 +0000

    fix(release):  remove custom pkgRoot for npm release

--- release.config.cjs
+++ release.config.cjs
@@ -8,12 +8,7 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    [
-      "@semantic-release/npm",
-      {
-        pkgRoot: "dist",
-      },
-    ],
+    "@semantic-release/npm",
     "@semantic-release/git",
   ],
 }
