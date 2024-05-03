e71cb15b6d70cbf42f1f7459c68f73b2ee3deb92
Mohamed Ibrahim <mo.ibrahim@akadenia.com>
2024-05-03 17:27:50 +0300

    feat(dete-helpers): added support for formatting date in short ordinal form

--- src/date.ts
+++ src/date.ts
@@ -39,3 +39,52 @@ export const formatDateTime = (
   options: Intl.DateTimeFormatOptions,
   localTimezone: string | Intl.Locale = "en-US",
 ): string => new Date(date).toLocaleDateString(localTimezone, options)
+
+/**
+ *
+ * @function
+ * @param {string | Date} date - The date string that needs to be returned as a Date object.
+ * @returns {Date} - A date that represents the new date object.
+ */
+
+export function getDate(rawDate: string | Date): Date {
+  return typeof rawDate === 'string' ? new Date(rawDate) : rawDate
+}
+
+/**
+ *
+ * @function
+ * @param {string | Date} date - The date string that needs to be formatted
+ * @returns {string} - A string representing the date in the short ordinal date
+ */
+
+export function getShortOrdinalDate(date: string | Date): string {
+  const newDate = getDate(date)
+  const day = newDate.getDate()
+  const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ]
+  const monthIndex = newDate.getMonth()
+  const year = newDate.getFullYear()
+
+  const ordinalEndings = ['th', 'st', 'nd', 'rd']
+  let suffix = ordinalEndings[0]
+  if (day <= 3 && day > 0) {
+    suffix = ordinalEndings[day]
+  } else {
+    // Determine the ordinal endings based on the last digit of the day
+    suffix = ordinalEndings[day % 10]
+  }
+  return `${day}${suffix}, ${monthNames[monthIndex]} ${year}`
+}
