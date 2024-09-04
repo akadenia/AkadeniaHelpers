90add6af84bc0383ae8f9e6a96170e8bd1566f63
Mohamed Ibrahim <eng.mohammedibrahim904@gmail.com>
2024-09-04 18:19:15 +0300

    feat(fileHelpers): add revokePreviewUrls and getFileExtenstionsFromTypes function

--- __tests__/file.test.ts
+++ __tests__/file.test.ts
@@ -1,6 +1,21 @@
 import { describe, it, expect } from "@jest/globals"
 
-import { checkFileExtension } from "../src/file"
+import revokeDiffFilePreviewUrls, { checkFileExtension, getFileExtensionsFromTypes } from "../src/file"
+
+async function isObjectURLRevoked(url: string) {
+  try {
+    const response = await fetch(url)
+    return !response.ok // If the response is not ok, the URL might be revoked
+  } catch (error) {
+    return true // Fetch failed, indicating URL is revoked
+  }
+}
+
+function generateFiles(length: number) {
+  return Array.from({ length }, (_, i) => i).map((i) => {
+    return new Blob(["Hello, world!"], { type: "text/plain" })
+  })
+}
 
 describe("checkFileExtension", () => {
   it("should return true if the file extension is valid", () => {
@@ -13,5 +28,59 @@ describe("checkFileExtension", () => {
 
   it("should return false if the file extension is missing", () => {
     expect(checkFileExtension("test", ["md"])).toBe(false)
+  })
+})
+
+describe("getFileExtensionsFromTypes", () => {
+  it("should return the extensions from the types", () => {
+    const types = ["images/png", "images/jpeg"]
+    const expected = ["png", "jpeg"]
+    expect(getFileExtensionsFromTypes(types)).toEqual(expected)
+  })
+
+  it("should throw error if types parameter is not an array", () => {
+    const types = undefined
+    const errorActual = () => getFileExtensionsFromTypes(types as unknown as Array<string>)
+    const expected = "types must be an array"
+    expect(errorActual).toThrow(expected)
+  })
+
+  it("should ignore any type value that is not valid in the types array", () => {
+    const types = ["images/png", "images/jpeg", "dummystring"]
+    const expected = ["png", "jpeg"]
+    expect(getFileExtensionsFromTypes(types)).toEqual(expected)
+  })
+})
+
+describe("revokeDiffFilePreviewUrls", () => {
+  const createObjectUrls = (files: Blob[]) => {
+    return files.map((file) => URL.createObjectURL(file))
+  }
+  it("should revoke urls that are not in the new list", async () => {
+    const files = generateFiles(3)
+    const oldPreviewUrls = createObjectUrls(files)
+    const newPreviewUrls = oldPreviewUrls.slice(1)
+    revokeDiffFilePreviewUrls(oldPreviewUrls, newPreviewUrls)
+    expect(await isObjectURLRevoked(oldPreviewUrls[0])).toBeTruthy()
+  })
+
+  it("should not revoke any urls when the old list is all in the new list", async () => {
+    const files = generateFiles(3)
+    const newPreviewUrls = createObjectUrls(files)
+    const oldPreviewUrls = newPreviewUrls.slice(1)
+    revokeDiffFilePreviewUrls(oldPreviewUrls, newPreviewUrls)
+    expect(
+      (await Promise.all(newPreviewUrls.map(async (url) => await isObjectURLRevoked(url)))).every((url) => !url),
+    ).toBeTruthy()
+  })
+  it("should throw an error when the new list is not an array", async () => {
+    const expectedError = "oldList and newList must be arrays"
+    const errorActual = () => revokeDiffFilePreviewUrls([], "" as unknown as Array<string>)
+    expect(errorActual).toThrow(expectedError)
+  })
+  it("should throw an error when the old list is not an array", async () => {
+    const expectedError = "oldList and newList must be arrays"
+    const errorActual = () => revokeDiffFilePreviewUrls("" as unknown as Array<string>, [])
+    expect(errorActual).toThrow(expectedError)
   })
 })
--- src/file.ts
+++ src/file.ts
@@ -11,3 +11,47 @@ export const checkFileExtension = (filePath: string, validExtensions: string[]):
   const fileExtension = filePath.split(".").pop()
   return !!fileExtension && validExtensions.includes(fileExtension)
 }
+
+/**
+ * Get the files extensions form the types passed
+ * It ignores any value tha is passed in the types array that is not a valid type
+ * @param types The array of types passed
+ * @returns The array of file extensions i.e. ["png"]
+ * @example
+ * getFileExtensionsFromTypes(["images/png", "images/jpeg"]) // ["png", "jpeg"]
+ */
+export const getFileExtensionsFromTypes = (types: string[]) => {
+  if (!Array.isArray(types)) {
+    throw new Error("types must be an array")
+  }
+  return types
+    .filter((type) => type.includes("/"))
+    .map((type) => type.trim().split("/").pop())
+    .reduce((acc, type) => {
+      if (type && !acc.includes(type)) {
+        acc.push(type)
+      }
+      return acc
+    }, [] as string[])
+}
+
+/**
+ * Gets two arrays of the file preview urls and revokes the urls that are not in the new list
+ * @param oldList The array of file preview urls for the old list
+ * @param newList The array of file preview urls for the new list
+ * @returns
+ * @example
+ * revokeDiffFilePreviewUrls([url1, url2], [url1]) => url2 is revoked
+ */
+
+export default function revokeDiffFilePreviewUrls(oldList: string[], newList: string[]) {
+  if (!(Array.isArray(oldList) && Array.isArray(newList))) {
+    throw new Error("oldList and newList must be arrays")
+  }
+  const differenceList = oldList.filter(
+    (currentOldPreviewUrl) => !newList.some((currentNewPreviewUrl) => currentNewPreviewUrl === currentOldPreviewUrl),
+  )
+  differenceList.forEach((oldUrl) => {
+    URL.revokeObjectURL(oldUrl)
+  })
+}
